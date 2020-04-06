027fb996ba026b2433d36b209056bea6e4040596
filipeAraujoPadua <filipearaujopadua@gmail.com>
2020-04-05 21:28:23 -0300

    projeto base

---
+++ src/main/java/basea/baseA/BaseAApplication.java
@@ -0,0 +1,13 @@
+package basea.baseA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basea/baseA/BaseAApplicationTests.java
@@ -0,0 +1,13 @@
+package basea.baseA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
